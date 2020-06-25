06ecea9470df915a70d648b1958612feba49cc72
Farid Siadatzade <43848075+faridsiadatzade@users.noreply.github.com>
2020-06-25 22:17:28 +0430

    Updates

---
+++ precache-manifest.9f823d1fb70976d61579770a31273686.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7680343eebc4fba2513d7909294d246",
+    "url": "/faridsi.ir/index.html"
+  },
+  {
+    "revision": "1fe6e699fdee3bab2c41",
+    "url": "/faridsi.ir/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "efea303e1f4bee6d8537",
+    "url": "/faridsi.ir/static/css/main.e62b2fd0.chunk.css"
+  },
+  {
+    "revision": "1fe6e699fdee3bab2c41",
+    "url": "/faridsi.ir/static/js/2.25106648.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/faridsi.ir/static/js/2.25106648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efea303e1f4bee6d8537",
+    "url": "/faridsi.ir/static/js/main.474e5f10.chunk.js"
+  },
+  {
+    "revision": "fae3c8aa351dad8f1cad",
+    "url": "/faridsi.ir/static/js/runtime-main.dbd61ba4.js"
+  },
+  {
+    "revision": "655bed2d732fc58c658534c795502fa9",
+    "url": "/faridsi.ir/static/media/Farid.655bed2d.jpg"
+  },
+  {
+    "revision": "e2c1df4e53f7f4c3d56cb4d138e637c0",
+    "url": "/faridsi.ir/static/media/artak-example-1.e2c1df4e.png"
+  },
+  {
+    "revision": "5eafba615dad10675bcd370ec5ef9258",
+    "url": "/faridsi.ir/static/media/artak-example-2.5eafba61.png"
+  },
+  {
+    "revision": "dbbf02e97b44fccdcfec6321b225a033",
+    "url": "/faridsi.ir/static/media/artak-example-3.dbbf02e9.png"
+  },
+  {
+    "revision": "bdacb5ff456d4b23e95d161e5f6b88d7",
+    "url": "/faridsi.ir/static/media/banner.bdacb5ff.png"
+  },
+  {
+    "revision": "d77158853fa04713a86865cb36afee31",
+    "url": "/faridsi.ir/static/media/delta-example-1.d7715885.png"
+  },
+  {
+    "revision": "956e749d17cc264323232749b24ad30b",
+    "url": "/faridsi.ir/static/media/delta.956e749d.ico"
+  },
+  {
+    "revision": "03acc261a4a92a60f992cec667043be5",
+    "url": "/faridsi.ir/static/media/farid-demo-1.03acc261.png"
+  },
+  {
+    "revision": "c2c39f00778c1f86b695f765d5d7f962",
+    "url": "/faridsi.ir/static/media/nokarto.c2c39f00.jpg"
+  },
+  {
+    "revision": "daea7491e9b3cb64f501dd6fc321197f",
+    "url": "/faridsi.ir/static/media/parham.daea7491.jpg"
+  },
+  {
+    "revision": "707afaf3079ed64bb15bdf3fd9230dd4",
+    "url": "/faridsi.ir/static/media/royal-demo-2.707afaf3.png"
+  },
+  {
+    "revision": "4fb348b8a8e4c7f662bb7fa7346b0296",
+    "url": "/faridsi.ir/static/media/royal.4fb348b8.png"
+  }
+]);
